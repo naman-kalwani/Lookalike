7ae87e52edeb16c9a056c6e4a8a7ed6599c449a0
naman <kalwani.naman1406@gmail.com>
2025-10-14 07:39:37 +0530

    commit for deploy

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+console.log(process.env.FRONTEND_URL, process.env.FRONTEND_PROD_URL);
+
+// Use env variables for CORS
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_PROD_URL,
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://your-frontend-domain.vercel.app",
-    ],
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
   })
 );
